535f9489ba75d0dc4d629c9c92198280767067d4
sstacker_m <jooxmeen2018@gmail.com>
2022-10-10 13:28:31 +0700

    edit announcement

---
+++ _news/announcement_1.d
@@ -0,0 +1,12 @@
+---
+layout: post
+title: Storage and Disposal of Radioactive Waste
+date: 2022-10-10 15:59:00-0400
+inline: false
+---
+
+Radioactive wastes are stored so as to avoid any chance of radiation exposure to people, or any pollution.
+
+***
+
+
